1652d3ae0df3e2698d885d7c6402fd9edd0bd07a
Marlon Mantilla Martinez <marman0315@gmail.com>
2016-09-26 23:05:24 -0500

    Adding /players endpoint for fetching footballer player information fro

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/players', function (req, res) {
+
+    res.setHeader('Content-Type', 'application/json');
+
+    var playerName = req.query.name;
+
+    var url = "https://en.wikipedia.org/wiki/" + playerName;
+
+    return request(url, function (error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+
+            var json = {name: "", dob: "", team: "", clubes: ""};
+
+            $('.infobox').filter(function () {
+                var data  = $(this);
+                json.name = data.find('.nickname').first().text();
+                json.dob  = data.find('.ForceAgeToShow').text();
+                json.team = data.find('.org').first().first().text();
+
+
+                var seniorCarrerEl = data.find('tr:contains("Senior career")');
+
+                var untilObject = data.find('tr:contains("National team")');
+
+                var carreerElements = seniorCarrerEl.next().nextUntil(untilObject);
+
+                var teams = [];
+                carreerElements.each(function (index, value) {
+                    var current = $(value);
+                    teams[index] = {
+                        years: current.children().find('span').text(),
+                        club: current.find('a').text()
+                    };
+                });
+                json.clubes = teams;
+
+            });
+
+
+            if ( json.name === "" || json.name == undefined ) {
+
+                $('.firstHeading').filter(function () {
+                    var data = $(this);
+                    json.name = data.text();
+                });
+
+            }
+
+            return res.send(json);
+        }else {
+            return res.send(error);
+        }
+    }); 
+
+});
+
+
+app.listen('8081');
+
+console.log('App running on localhost:8081');
+
+exports = module.exports = app;
