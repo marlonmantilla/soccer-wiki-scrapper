3f886ba0b7f36ccc77ef454b565437c8ea225cbf
Marlon Mantilla Martinez <marman0315@gmail.com>
2016-09-28 23:41:02 -0500

    Adding cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var cors    = require('cors');
+
+app.use(cors());
 
 app.get('/players', function (req, res) {
 
